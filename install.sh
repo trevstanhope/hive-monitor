a0529ee67a373c01503abf2dffa9581c2510a1c0
Trevor Stanhope <trevor@TS130>
2014-01-08 18:12:45 -0500

    changes

--- install.sh
+++ install.sh
@@ -5,18 +5,16 @@ sudo apt-get install -y couchdb isc-dhcp-server arduino arduino-mk
 sudo apt-get install -y python-serial python-cherrypy3 python-couchdb python-numpy python-pyaudio python-alsaaudio
 
 ## Setup adhoc network
-sudo mv interfaces /etc/network/interfaces
+sudo mv config/interfaces /etc/network/interfaces
 sudo cp -f config/dhcpd.conf /etc/dhcp/
 
 ## Setup start on boot
 sudo cp -f config/rc.local /etc/
-sudo cp -f HiveMind.py /usr/bin/
 
 ## Setup CouchDB
 sudo cp -f config/local.ini /etc/couchdb/
     
 ## Setup Arduino
-cd config/alamode-setup && sudo ./setup
 sudo cp -rf libs/* /usr/share/arduino/libraries
 
 ## Reboot
