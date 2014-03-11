71450263ff5d3eb08471b2db1cc50eb69cd51ef0
Trevor Stanhope <trevor@W510>
2014-03-11 12:57:13 -0400

    compile  to arduino

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/bi/sh
+
 ## Dependencies
 sudo apt-get update
-sudo apt-get install -y couchdb isc-dhcp-server arduino arduino-mk
-sudo apt-get install -y python-serial python-cherrypy3 python-couchdb python-numpy python-pyaudio python-alsaaudio
-
-## Setup adhoc network
-sudo mv config/interfaces /etc/network/interfaces
-sudo cp -f config/dhcpd.conf /etc/dhcp/
-
-## Setup start on boot
-sudo cp -f config/rc.local /etc/
-
-## Setup CouchDB
-sudo cp -f config/local.ini /etc/couchdb/
+sudo apt-get install -y arduino arduino-mk
     
 ## Setup Arduino
 sudo cp -rf libs/* /usr/share/arduino/libraries
@@ -26,6 +16,11 @@ sudo cp config/80-alamode.rules /etc/udev/rules.d
 sudo chown root /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo chgrp root /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
 sudo chmod a+s /usr/bin/avrdude /usr/share/arduino/hardware/tools/avrdude
+
+## Compile to Arduino
+make clean
+make
+make upload
 
 ## Reboot
 sudo reboot
